7c1be739e8d4226b0f2962faf3036afe4f2c5b2a
rahulpadaliya8787-sys <rahulpadaliya8787@gmail.com>
2025-12-31 20:06:59 +0530

    Update server.js

--- server.js
+++ server.js
@@ -82,7 +82,7 @@ app.post("/submit-inquiry", async (req, res) => {
       }
     };
 
-    const zohoURL = `https://creator.zoho.in/api/v2/${process.env.ZOHO_OWNER}/${process.env.ZOHO_APP_LINK}/form/${process.env.ZOHO_FORM_LINK}`;
+    const zohoURL = `https://creator.zoho.in/api/v2/${process.env.ZOHO_OWNER}/${process.env.ZOHO_APP_LINK}/form/${process.env.ZOHO_FORM_LINK}/records`;
 
     const zohoRes = await fetch(zohoURL, {
       method: "POST",
